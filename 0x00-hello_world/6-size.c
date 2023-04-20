321b02151f2a5f5db06e1f15916ed3b05003950a
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-20 12:28:21 -0700

    :

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Datatypes
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+long long o;
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long));
+printf("Size of a long long int: %i byte(s)\n", sizeof(o));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
+return (0);
+}
