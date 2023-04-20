59e1e19f4f9d3167d1c35ac68a81594deccff480
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-20 11:19:30 -0700

    :

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * print puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char puts2[] = "Programming is like building a multilingual puzzle";
+char puts2[] = "\"Programming is like building a multilingual puzzle";
 puts(puts2);
 return (0);
 }
