19b9e55fd65a9d48f9043abda81ac8212690681f
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-22 22:13:33 +0200

    :

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,6 @@
 */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
 char alphapet ;
 for(alphapet = 'a'; alphapet <= 'z'; alphapet++)
 putchar(alphapet);
