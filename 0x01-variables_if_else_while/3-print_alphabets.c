f38350b2e461cfacfd7f8ce973c3c82537567727
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-22 23:10:39 +0200

    :

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *putchar() is a function
+ * Return: Always 0 (Success)
+*/
+int main()
+{
+char s;
+char u;
+for(s = 'a'; s <= 'z'; s++)
+putchar(s);
+        
+for(u = 'A'; u <= 'Z'; u++)
+putchar(u);
+        
+putchar('\n');
+return 0;
+}
