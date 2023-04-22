d992056d246ecb9dc5ff17cf21dff91f92c10e27
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-22 14:03:22 +0200

    :

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%i is positive", n);
-} else if (n == 0)
+printf("%i is positive\n", n);
+}
+else if (n == 0)
 {
-printf("%i is zero", n);
-} else
+printf("%i is zero\n", n);
+} 
+else
 {
-printf("%i is negative", n);
+printf("%i is negative\n", n);
 }
 return (0);
 }
