5a46aa4c25829f554fbcb9ef21ceb58d759cd1c4
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-24 16:45:23 +0200

    :

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *putchar() is a function
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int firstdigit = 0 , secondigit;
+while (firstdigit <= 99)
+{
+secondigit = firstdigit;
+while (secondigit <= 99)
+{
+if (secondigit != firstdigit)
+{
+putchar((firstdigit / 10) + 48);putchar((firstdigit % 10) + 48);
+putchar(' ');
+putchar((secondigit / 10) + 48);putchar((secondigit % 10) + 48);
+if (firstdigit != 98 || secondigit != 99)
+{
+putchar(',');
+putchar(' ');      
+}
+}          
+secondigit++;
+}
+firstdigit++;
+}
+putchar('\n');
+return (0);
+}
