93febe80624fa09b9f168adb59cfb2cbf9590d30
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-25 14:58:29 -0700

    :

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,31 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - check the code
- *
- * Return: Always 0.
+ * functoinprint_alphabet
+ * Return: putchari
  */
-<<<<<<< HEAD
 
-=======
-void print_alphabet();
-int main(void)
-{
-print_alphabet();
-return (0);
-}
->>>>>>> 2ea1a4dff1d81a7de09e195cd8850d1424640e67
 void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
 {
-<<<<<<< HEAD
 _putchar(i);
 }
 _putchar('\n');
-=======
-printf("%c",i);
->>>>>>> 2ea1a4dff1d81a7de09e195cd8850d1424640e67
 }
-void print_alphabet(void);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,2 @@
-<<<<<<< HEAD
-int _putchar(char c);
+int _putchar(char i);
 void print_alphabet(void);
-=======
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-#endif
->>>>>>> 2ea1a4dff1d81a7de09e195cd8850d1424640e67
