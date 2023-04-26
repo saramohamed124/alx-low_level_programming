bc31c5f916a3e693747d02604dff3cd5ca54127f
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-26 10:38:56 +0200

    :

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *putchar() is a function
+ * Return: Always 0 (Success)
+*/
 /**
  * _islower : Checked for  chracters lower
  * @c : The chracter to be checked
