5b492060e4a02d35fa65c05ef9ff34de65a280ee
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-26 21:10:55 +0200

    :

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* jack_bauer - print the last digit of int
+ * Return: ....
+ */
+void jack_bauer(void)
+{
+int a, b, c, d;
+for (a = 0; a <= 2; a++)
+{
+for (a = 0; a <= 9; a++)
+{
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+for (c = 0; c <= 5; c++)
+{
+for (d = 0; d <= 9; d++)
+{
+_putchar(a + 48);
+_putchar(b + 48);
+_putchar(45);
+_putchar(c + 48);
+_putchar(d + 48);
+_putchar('\n');
+}
+}
+}
+}
+}
+}
