ce1cd622e91d0eed3b57d4091e27833d66db76d4
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-26 14:53:46 +0200

    :

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ * int: The character to be checked
+ * Return: 1 for letter, lowercase and uppercase characters and 0 for else
+ */
+int _abs(int)
+{
+  int x;
+  x = abs(-1);
+printf("%i\n", x);
+ x= abs(0);
+printf("%i\n", x);
+  x = abs(1);
+ printf("%i\n", x);
+  x = abs(-98);
+ printf("%i\n", x);
+}
