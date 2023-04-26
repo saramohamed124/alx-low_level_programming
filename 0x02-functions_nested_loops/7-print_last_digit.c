334e9bcac01f85b10f1b7d729ce61cd92c3e2284
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-26 17:38:56 +0200

    :

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_last_digit - Computes the absolute value of an integer.
+ * @n: The character to be checked
+ * Return: 1 for letter, lowercase and
+ uppercase characters and 0 for else
+ */
+ int print_last_digit(int r)
+ {
+ int last_digit;
+ if (last_digit < 0)
+ {
+   last_digit = -1 * (n % 10);
+ }
+ else
+ {
+ last_digit = n % 10;
+ }
+ _putchar(last_digit + 48);
+ return (last_digit);
+ }
