d596907a1870a6ea7c06ff622f5f1d87d24cd2a0
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-27 01:25:45 +0200

    b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - add two value
  * @x - the first number to computed
- * @z - the secound number to computed
+ * @y - the secound number to computed
  * Return : integer value
  */
 int add(int x, int y)
