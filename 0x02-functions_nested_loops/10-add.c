dfc0264731ee09a6763f2c90dd95af2fdd9ab14d
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-27 01:30:27 +0200

    b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Add two valuese
- * x - The first number to computed
- * y - The secound number to computed
+ * @x - The first number to computed
+ *@y - The secound number to computed
  * Return: integer value
  */
 int add(int x, int y)
