d8b20558fb4222d64d82f4b418c8c30f3bbe5692
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-27 14:16:44 +0200

    :

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *if is a f
+ * Return: Always 0 (Success)
+*/
 void positive_or_negative(int i)
 {
 if (i > 0)
