8e26f416327dae107788ae0104e97bf64bd4f3ca
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-27 02:40:24 +0200

    :

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * starting from
  * @n: The value of timrs table
  */
-void times_table(int n)
+void print_times_table(int n)
 {
 int num, mult, prod;
 if (n >= 0 && n <= 15)
@@ -23,7 +23,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
-_putchar(((prod / 10)) + 48);
+_putchar(((prod / 10)) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
 {
