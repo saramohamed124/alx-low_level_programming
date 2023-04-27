46fbb0090ef574bb75cdb5db9b96aab34b2e2c22
saramohamed124 <saramohamaden1234@gmail.com>
2023-04-27 16:16:59 -0700

    :

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,12 @@
 void more_numbers(void)
 {
 int p, i;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (p = 0; p <= 14; p++)
 {
-if (p > 9){
+if (p > 9)
+{
 _putchar((p / 10) + 48);
 }
 _putchar((p % 10) + 48);
