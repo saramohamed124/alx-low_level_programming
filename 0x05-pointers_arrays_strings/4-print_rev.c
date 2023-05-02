cc2106a124567fda533386c70f00bfdc0166edac
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-02 10:07:56 -0700

    :

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse
+ * @s : the string to print
+ *  Return : 0 always success
+ */
+void print_rev(char *str)
+{
+int s = 0;
+while (str[s] != 0)
+{
+s++;
+}
+for(s -= 1; s >= 0; s--)
+{
+putchar(str[s]);
+}
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * rev_string - prints a string in reverse
+ * @s : the string to print
+ *  Return : 0 always success
+ */
+void rev_string(char *s)
+{
+strrev(s);
+}
