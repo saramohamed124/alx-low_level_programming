927901c70cbefea4bcf099e0eba079f4b40621a1
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-02 07:46:34 -0700

    :

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *  Return : 0 always success
+ */
+void _puts(char *str)
+{
+printf("%sn", str);
+}
