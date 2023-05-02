008d3734bf9ea5be04a4b0f5d30d629bcbbb2a69
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-02 10:10:45 -0700

    :

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 #include <string.h>
 /**
  * print_rev - prints a string in reverse
- * @s : the string to print
+ * @str : the string to print
  *  Return : 0 always success
  */
 void print_rev(char *str)
@@ -13,8 +13,9 @@ while (str[s] != 0)
 {
 s++;
 }
-for(s -= 1; s >= 0; s--)
+for (s -= 1; s >= 0; s--)
 {
 putchar(str[s]);
 }
+putchar('\n');
 }
