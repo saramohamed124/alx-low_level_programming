67dc50e79dd46ac314724c999da52d17c45407ee
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-02 19:12:55 -0700

    :

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 /**
  * rev_string - prints a string in reverse
  * @s : the string to print
@@ -8,5 +6,14 @@
  */
 void rev_string(char *s)
 {
-strrev(s);
+int len = 0, index = 0;
+char tmp;
+while (s[index++])
+len++;
+for (index = len - 1; index >= len / 2; index--)
+{
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
+}
 }
