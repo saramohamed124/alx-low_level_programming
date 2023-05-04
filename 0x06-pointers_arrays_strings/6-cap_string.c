7e014a4d0c04c695765793aea801a517080d0575
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-04 09:53:07 -0700

    :

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,20 @@ while (str[text])
 {
 while (!(str[text] >= 'a' && str[text] <= 'z'))
 text++;
-if (str[text - 1] == ' ' || str[text - 1] == '\n' || str[text - 1] == ',' || str[text - 1] == '"' || str[text - 1] == '\t' || str[text - 1] == ':' || str[text - 1] == ';' || str[text - 1] == '?' || str[text - 1] == '.' || str[text - 1] == '(' || str[text - 1] == ')' || str[text - 1] == '{' || str[text - 1] == '}' || text == 0)
+if (str[text - 1] == ' ' ||
+str[text - 1] == '\n' ||
+str[text - 1] == ',' ||
+str[text - 1] == '"' || 
+str[text - 1] == '\t' || 
+str[text - 1] == ':' || 
+str[text - 1] == ';' || 
+str[text - 1] == '?' || 
+str[text - 1] == '.' || 
+str[text - 1] == '(' || 
+str[text - 1] == ')' || 
+str[text - 1] == '{' || 
+str[text - 1] == '}' || 
+text == 0)
 str[text] -= 32;
 text++;
 }
