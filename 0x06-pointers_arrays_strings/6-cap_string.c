54441f2c5242dc7ffc69550fa4af589c2dd6371f
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-04 09:55:07 -0700

    :

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,16 +14,16 @@ text++;
 if (str[text - 1] == ' ' ||
 str[text - 1] == '\n' ||
 str[text - 1] == ',' ||
-str[text - 1] == '"' || 
-str[text - 1] == '\t' || 
-str[text - 1] == ':' || 
-str[text - 1] == ';' || 
-str[text - 1] == '?' || 
-str[text - 1] == '.' || 
-str[text - 1] == '(' || 
-str[text - 1] == ')' || 
-str[text - 1] == '{' || 
-str[text - 1] == '}' || 
+str[text - 1] == '"' ||
+str[text - 1] == '\t' ||
+str[text - 1] == ':' ||
+str[text - 1] == ';' ||
+str[text - 1] == '?' ||
+str[text - 1] == '.' ||
+str[text - 1] == '(' ||
+str[text - 1] == ')' ||
+str[text - 1] == '{' ||
+str[text - 1] == '}' ||
 text == 0)
 str[text] -= 32;
 text++;
