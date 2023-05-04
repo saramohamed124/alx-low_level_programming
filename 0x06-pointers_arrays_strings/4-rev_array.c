59c20054a69cfd7e64ff7eef8b6e34f8db9122ea
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-04 08:36:31 -0700

    :

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ int w, integer;
 for (integer = n - 1; integer >= n / 2; integer--)
 {
 w = a[n - 1 - integer];
-a[n - 1- integer] = a[integer];
+a[n - 1 - integer] = a[integer];
 a[integer] = w;
 }
 }
