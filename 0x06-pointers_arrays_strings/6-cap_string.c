d55f689eae125bef565f2423938619eb2d25a2a7
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-04 09:48:21 -0700

    :

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * cap_string -  concatenates two strings
+ * @str : it is value
+ * Return: dest
+ */
+char *cap_string(char *str)
+{
+int text = 0;
+while (str[text])
+{
+while (!(str[text] >= 'a' && str[text] <= 'z'))
+test++;
+if (str[text - 1] == ' ' || str[text - 1] == '\n' || str[text - 1] == ',' || str[text - 1] == '"' || str[text - 1] == '\t' || str[text - 1] == ':' || str[text - 1] == ';' || str[text - 1] == '?' || str[text - 1] == '.' || str[text - 1] == '(' || str[text - 1] == ')' || str[text - 1] == '{' || str[text - 1] == '}' || text == 0)
+str[text] -= 32;
+text++;
+}
+return (str);
+}
