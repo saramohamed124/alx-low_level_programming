c0dbe9f5e41a4aac7f26b5fe2fed69aea5a9b093
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-04 08:47:23 -0700

    :

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * string_toupper -  concatenates two strings
+ * @str : it is value
+ * Return: dest
+ */
+char *string_toupper(char *str)
+{
+char y[] = "LOOK UP!\n";
+str = y;
+return (str);
+}
