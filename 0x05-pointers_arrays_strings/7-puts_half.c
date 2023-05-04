2f50d1bb67befdd0ea2d0316446fe1083aac187c
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-04 08:55:08 -0700

    :

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+/**
+ * puts_half - prints a string in reverse
+ * @str : the string to print
+ *  Return : 0 always success
+ */
+void puts_half(char *str)
+{
+    int n;
+int s = strlen(str);
+if (n == (len(s) -1) / 2)
+{
+    printf("%s", str);
+}
+}
