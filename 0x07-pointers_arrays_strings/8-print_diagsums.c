1c7e22d53693e7ad3c58abdc04815b95b1139c93
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-08 15:22:49 -0700

    :

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,9 +10,9 @@
 void print_diagsums(int *a, int size)
 {
 int i, n, sun1 = 0, sun2 = 0;
-for (i = 0; i <= (size * size); i = i + size - 1)
+for (i = 0; i <= (size * size); i = i + size + 1)
 sun1 = sun1 + a[i];
-for (n = size - 1; n <= (size * size) - size; n = n + size -1)
+for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 sun2 = sun2 + a[n];
 
 printf("%d, %d\n", sun1, sun2);
