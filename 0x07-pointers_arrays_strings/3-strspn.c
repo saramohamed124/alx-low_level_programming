1c48435d5766bc8b8b05be9f93bcec1f64f14eb2
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-08 14:31:25 -0700

    :

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ *_strspn -  fills memory with a constant byte
+ * @s : it is value
+ * @accept : it is value
+ * Return: 0 always success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int o;
+    o = strspn(s, accept);
+    return (o);
+}
