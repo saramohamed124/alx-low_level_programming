cd0e6bb360a11c7647f55421c73cbdd9def98985
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-08 15:27:18 -0700

    :

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *set_string -  fills memory with a constant byte
+ * @s : it is value
+ * @to : it is a value
+ * Return: 0 always success
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
