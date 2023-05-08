773794a15e7503d988c55cdc770d4abd3ca26fcd
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-08 12:30:56 -0700

    :

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_memset -  fills memory with a constant byte
+ * @s : it is value
+ * @b : it is value
+ * @n : it is a value
+ * Return: 0 always success
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+memset(s, b,n);
+return(0);
+}
