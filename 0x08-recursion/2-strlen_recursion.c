78089417a387944d7ad2ad599f025c5b61229c80
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-10 14:46:44 -0700

    :

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strlen_recursion - prints a string, followed by a new line
+ *@s : it is a value
+ *Return : 0 always Success
+*/
+int _strlen_recursion(char *s)
+{
+int n;
+n = strlen(s);
+return (n);
+}
