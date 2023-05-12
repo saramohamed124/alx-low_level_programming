475f04d2c6ae1dafcf02c62413fe329259d3d887
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-12 22:47:51 +0200

    :

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * argc: it is integer value
- * argv: it is integer value
+ * 
  * Return: Always 0 (Success)
+ */
+/**
+ * argc: it is integer value
+ * argv: it is string value
  */
 int main(int argc, char *argv[])
 {
