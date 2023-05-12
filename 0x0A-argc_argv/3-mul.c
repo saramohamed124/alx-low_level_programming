f731bf55bf9669b05483b7a2a8f14dbedc37c339
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-13 01:14:46 +0200

    :

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: it is integer value
+ * @argv: it is string value
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int i, res;
+if (argc > 1)
+{
+
+res = atoi(argv[1]) * atoi(argv[2]);
+printf("%d", res);
+}
+else
+{
+printf("Error");
+}
+putchar('\n');
+return (0);
+}
