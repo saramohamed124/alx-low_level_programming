f30ca16884e72770f80217261d7b14fdf3b39c9e
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-12 23:09:39 +0200

    :

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: it is integer value
+ * @argv: it is string value
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d", argc);
+putchar('\n');
+return (0);
+}
