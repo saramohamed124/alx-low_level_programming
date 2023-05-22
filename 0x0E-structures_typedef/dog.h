b520acda0089859ae2d8efc9bb45785a7f3cda01
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-22 16:41:16 -0700

    :

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
-#ifdef DOG_H
-#define  DOG_H
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - check the code
+ * struct dog - typedef for struct dog
  *@name: it is value
  *@age: it is value
  *@owner: it is value
@@ -13,4 +13,10 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
