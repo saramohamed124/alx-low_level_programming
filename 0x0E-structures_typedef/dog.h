3688b044b8afdd9650b0bafacf2ca3e00db5f48d
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-22 15:44:52 -0700

    :

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifdef DOG_H
+#define  DOG_H
+/**
+ * struct dog - check the code
+ *@name: it is value
+ *@age: it is value
+ *@owner: it is value
+ * Description: Define a new type struct dog with the following elements:
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
