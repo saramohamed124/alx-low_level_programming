e0244597e7d7f09b3eec873e0d97da8552873431
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-24 13:39:42 -0700

    :

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size: it is value
+ * @action: it is value
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int *end = array + size - 1;
+if (array && size && action)
+	while(array <= end)
+		action(*array++);
+}
