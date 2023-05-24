06c63f3a4a3db6e5296ae5965f237d4ec81670ba
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-24 14:52:08 -0700

    :

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,49 +1,54 @@
 #include "3-calc.h"
 /**
- * op_add - Struct op
+ * op_add - adds two integers
  *
  * @a: The operator
  * @b: The function associated
+ * Return: the remainder
  */
 int op_add(int a, int b)
 {
 return (a + b);
 }
 /**
- * op_sub - Struct op
+ * op_sub - subtracts two integers
  *
  * @a: The operator
  * @b: The function associated
+ * Return: the remainder
  */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 /**
- * op_mul - Struct op
+ * op_mul - multiplies two integers
  *
  * @a: The operator
  * @b: The function associated
+ * Return: the remainder
  */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
 /**
- * op_div - Struct op
+ * op_div - divide two integers
  *
  * @a: The operator
  * @b: The function associated
+ * Return: the remainder
  */
 int op_div(int a, int b)
 {
 return (a / b);
 }
 /**
- * op_mod - Struct op
+ * op_mod - modulo two integers
  *
  * @a: The operator
  * @b: The function associated
+ * Return: the remainder
  */
 int op_mod(int a, int b)
 {
