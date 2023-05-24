03fe0e7d7f27ce97af34231263b14b57994b9d44
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-24 14:13:38 -0700

    :

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - Struct op
+ *
+ * @a: The operator
+ * @b: The function associated
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - Struct op
+ *
+ * @a: The operator
+ * @b: The function associated
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - Struct op
+ *
+ * @a: The operator
+ * @b: The function associated
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - Struct op
+ *
+ * @a: The operator
+ * @b: The function associated
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - Struct op
+ *
+ * @a: The operator
+ * @b: The function associated
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
