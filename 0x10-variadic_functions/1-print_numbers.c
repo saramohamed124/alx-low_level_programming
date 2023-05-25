604656459463468842251067ef8c5d7025393145
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-25 14:13:12 -0700

    :

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - return the sum of all its parameters
+ *@separator: it is value
+ *@n: it is value
+ * Return: always 0 success
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+int  i = n;
+
+va_list ap;
+if (!n)
+{
+printf("\n");
+return;
+}
+
+va_start(ap,n);
+while(i--)
+printf("%d%s", va_arg(ap,int),
+i ? (separator ? separator : "") : "\n");
+
+va_end(ap);
+}
