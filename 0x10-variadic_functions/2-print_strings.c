26dac83715993eea765963d9fc48d9d0ff13ae7c
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-25 14:22:45 -0700

    :

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,10 +18,10 @@ printf("\n");
 return;
 }
 
-va_start(ap,n);
-while(i--)
+va_start(ap, n);
+while (i--)
 printf("%s%s", (str = va_arg(ap, char *)) ? str : "nil",
 i ? (separator ? separator : "") : "\n");
 
-va_end(ap);
+va_end(ap)
 }
