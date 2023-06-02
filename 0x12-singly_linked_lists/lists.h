4f72ae983d464c8e745c3b8e1573716c30a0bd7b
saramohamed124 <saramohamaden1234@gmail.com>
2023-06-02 06:39:05 -0700

    :

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length
+ * @next: points to the next node
+ *
+ * Description: singly linked list 
+ * for Holberton project
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+int _strlen(char *s);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);void free_list(list_t *head);
+void _constructor(void) __attribute__ ((constructor));
+#endif
