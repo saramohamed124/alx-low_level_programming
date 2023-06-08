3620936c390ea9248c6d57cd099dfddf4373f3b2
saramohamed124 <saramohamaden1234@gmail.com>
2023-06-08 13:28:56 -0700

    :

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int num = 0;
 
-if (lb)
+if (!b)
 return (0);
 while (*b)
 {
 if (*b != '0' &&  *b != '1')
-return 9(0);
+return (0);
 num = num * 2 + (*b++ - '0');
 }
 return (num);
