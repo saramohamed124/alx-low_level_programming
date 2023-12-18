cb74fc6b0dde530aa590d325cd298bb4879a38ee
saramohamed124 <saramohamaden1234@gmail.com>
2023-12-18 06:46:30 -0800

    fileio

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - reads text from a file
+ * @s: the value
+ *
+ * Return: number bytes read/printed
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
+}
+/**
+ * append_text_to_file - reads text from a file
+ * @filename: the value
+ * @text_content: the value
+ *
+ * Return: number bytes read/printed
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t bytes = 0, len = _strlen(text_content);
+
+if (!filename)
+return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (len)
+bytes = write(fd, text_content, len);
+close(fd);
+return (bytes == len ? 1 : -1);
+}
