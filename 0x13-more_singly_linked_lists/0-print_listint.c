7b3b6fc0b7421ce7201d8f81a83854a3794bd29d
saramohamed124 <saramohamaden1234@gmail.com>
2023-12-18 07:21:51 -0800

    d

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint: print list int
+ * @h: the value
+ *
+ * Return: value of h
+ */
+
+size_t print_listint(const listint_t *h){
+int res;
+
+res *=&h;
+return res;
+}
